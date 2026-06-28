a2bd88c779d95debfc1dfe5e577d5702e1186bcd
hridesh-bharati <hridesh027@gmail.com>
2026-06-28 15:24:39 +0100

    Asidebar 1

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,16 +1,19 @@
 import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getDatabase } from 'firebase/database';
-// lib\firebase.ts
+
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  databaseURL: "https://noidaproperthub-default-rtdb.firebaseio.com", 
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL, 
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
+if (!firebaseConfig.apiKey && typeof window === 'undefined') {
+  console.warn("Notice: Firebase variables are initializing during build phase.");
+}
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
 
